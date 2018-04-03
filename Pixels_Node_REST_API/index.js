e62d8d05592f3048ef5978e72b4cfc0ca000c85b
ManojkRajasekar <manoj.k.rajasekar@gmail.com>
2018-04-02 21:35:58 -0400

    Updated with changes

--- Pixels_Node_REST_API/index.js
+++ Pixels_Node_REST_API/index.js
@@ -1,65 +1,89 @@
-const mysql = require('mysql');
+// const mysql = require('mysql');
 const express = require('express');
 const app = express();
+const con = require('./connection');
+const cloudinary = require('cloudinary');
+// const getpostsbytopic = require('./posts');
+const posts = require('./posts');
+
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+const port = 8080;
+app.listen(port);
 
-const con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Rajasekar*1',
-    port:3306,
+cloudinary.config({
+    cloud_name:'pixelsapp',
+    api_key: '921914376421383',
+    api_secret: 'Kcsx9wMBbZVG8O-raoth2N76ByM'
 });
 
-con.connect((err) => {
-       if (err) throw err;
-       console.log("Connected!");
-// //      // con.query('CREATE DATABASE NODEDEMO', function(err, result){
-// //     //     if(err) throw err;
-// //     //     console.log('Database Created');
-// //     // });
-  });
+console.log('port running on 8080');
 
- const port = 8080;
- app.listen(port);
-
- console.log('port running on 8080');
-
-    con.query('USE sample_photoapp;', (err, result) => {
+    con.query('USE photoapp;', (err, result) => {
          if (err) throw err;
-         console.log('Using the sample_photoapp database');
+         console.log('Using the photoapp database');
      });
 
- app.get('/', (req, res) => {
-     res.status(201).json({
-         message:'This is Cool'
-     });
- });
 
- app.get('/getallposts', (req, res) => {
-     con.query('CALL Display_Posts()', (err, result) => {
-         if (err) throw err;
-          //console.log('Called SPS');
-         console.log(result);
-         res.status(201).json({
-             message: result
-         });
-     });
- });
-
-
- app.get('/getpostsbyid/:id', (req, res) => {
-    con.query('CALL Get_Posts_By_id(?)', [req.params.id],  (err, result) => {
-        if(err) throw err;
-        console.log(result);
-        res.status(201).json({
-            result
-        });
+    app.get('/getpostsbytopic/:topic_id', (req, res) => {
+        if(!isNaN(req.params.topic_id)) {
+            posts.getpostsbytopic(req.params.topic_id).then((result) => {
+                console.log(result);
+                res.status(201).json({
+                    result
+                });
+            }).catch((err) => { throw err; });
+        }
+        else{
+            console.log('Not null failed');
+        }
     });
-});
+
+    app.get('/getinitialinfo/:user_id', (req, res) => {
+        if(!isNaN(req.params.user_id)) {
+            posts.getinitialinfo(req.params.user_id).then((result) =>{
+                console.log(result);
+            }).catch((err) => { throw err });
+        }
+        else{
+            console.log('Not null failed');
+        }
+    });
+
+    // app.get('/getimage', (req, res) => {
+    //     res.send(
+    //         //cloudinary.image("https://upload.wikimedia.org/wikipedia/commons/0/0c/Scarlett_Johansson_Césars_2014.jpg", {type: "fetch"})    
+    //         cloudinary.image("https://upload.wikimedia.org/wikipedia/commons/0/0c/Scarlett_Johansson_Césars_2014.jpg", {type: "fetch"})
+    //     );
+    // });
+    
+    
+    
+
+//  app.get('/getallposts', (req, res) => {
+//      con.query('CALL Display_Posts()', (err, result) => {
+//          if (err) throw err;
+//           //console.log('Called SPS');
+//          console.log(result);
+//          res.status(201).json({
+//              message: result
+//          });
+//      });
+//  });
 
 
-module.exports = con;
+//   app.get('/getpostsbyid/:id', (req, res) => {
+//      con.query('CALL Get_Posts_By_id(?)', [req.params.id],  (err, result) => {
+//          if(err) throw err;
+//          console.log(result);
+//          res.status(201).json({
+//              result
+//          });
+//      });
+//  });
+
+
+// module.exports = con;
