78f3d7acc7985ae2ea3956a1d26b59c11655cfa4
ManojkRajasekar <manoj.k.rajasekar@gmail.com>
2018-04-10 20:28:29 -0400

    add vote functionality

--- database/votes.db.js
+++ database/votes.db.js
@@ -1,22 +1,10 @@
 const con = require('./utils/connection');
 const processor = require("./utils/processers");
 
-// const AddVote = (postid, userid) => {
-//     return new Promise((resolve, reject) => {
-//         query_str = 'CALL add_vote(?, ?, @out); SELECT @out as vote_id' ;
-//         con.query(query_str, [postid, userid], processor.processAddVoteResults(resolve, reject));
-//     });
-// };
-
-// const CheckIfValidVote = (postid, userid) => {
-//     return new Promise((resolve, reject) => {
-//         con.query('CALL get_valid_vote(?, ?)', [postid, userid], processor.processValidVoteResults(resolve, reject));
-//     });
-// };
 
 const AddVote = (postid, userid) => {
     return new Promise((resolve, reject) => {
-        con.query('CALL add_valid_vote(?, ?)', [postid, userid], processor.processAddVoteResults(resolve, reject));
+        con.query('CALL add_vote(?, ?)', [postid, userid], processor.processAddVoteResults(resolve, reject));
     });
 };
 
