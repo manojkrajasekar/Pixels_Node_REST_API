ada0dea03407312dafd419191a415a65b95edb3a
ManojkRajasekar <manoj.k.rajasekar@gmail.com>
2018-04-17 07:59:22 -0400

    Updated with updateVote api

--- database/votes.db.js
+++ database/votes.db.js
@@ -2,9 +2,10 @@ const con = require('./utils/connection');
 const processor = require("./utils/processers");
 
 
-const updateVote = (postid, userid, voteid) => {
+const updateVote = (postid, userid, voteid, isactive) => {
     return new Promise((resolve, reject) => {
-        con.query('SET @vote_id = ?; CALL update_vote(?, ?, @vote_id); SELECT @vote_id as vote;', [voteid, postid, userid], processor.processVoteResults(resolve, reject));
+        con.query('SET @vote_id = ?, @is_active = ?; CALL update_vote(?, ?, @vote_id, @is_active); SELECT @vote_id as voteId; SELECT @is_active as isActive',
+                    [voteid, postid, userid], processor.processVoteResults(resolve, reject));
     });
 };
 
