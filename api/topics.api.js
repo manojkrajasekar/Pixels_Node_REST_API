b9185326c6c98b8fad9e8366633e0ea36f1987a9
ManojkRajasekar <manoj.k.rajasekar@gmail.com>
2018-04-20 17:46:06 -0400

    Added getTopic

---
+++ api/topics.api.js
@@ -0,0 +1,46 @@
+const topics = require('../database/topics.db');
+const logger = require("../utils/logger");
+const utils = require("./utils");
+
+getTopic = (req, res) => {
+    let topicId = req.params.topic_id;
+    let limit = req.params.limit;
+    let errorMessage;
+    
+    // Validate the incoming request params
+    if(isNaN(topicId)){
+        errorMessage = "Topic Id should be an integer"
+    } 
+    else if(isNaN(limit)){
+        errorMessage = "limit should be an integer"
+    }
+
+    if (errorMessage != undefined) {
+        logger.error(errorMessage);
+        res.status(500).json({ 
+            error: {
+                message: errorMessage
+            }
+        });
+
+        return;
+    }
+
+    topics
+        .getTopics(topicId, limit)
+        .then((result) => {
+                logger.info(req, result);
+
+                currentTopic = utils.arrayFirstElementToObject(result);
+                res.status(201).json({currentTopic});
+            })
+            .catch((error) => { 
+                logger.error(req, error);
+                res.status(500).json({ error });
+            });
+} 
+
+
+module.exports = {
+    getTopic
+};
