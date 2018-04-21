6358e0af6c2789ef4ae338d5a3fc0107b62eaa91
ManojkRajasekar <manoj.k.rajasekar@gmail.com>
2018-04-21 09:39:35 -0400

    Updated

--- database/topics.db.js
+++ database/topics.db.js
@@ -1,10 +1,10 @@
 const con = require('./utils/connection');
 const processor = require("./utils/processers");
 
-const getTopic = (isCurrent, limit) => {
+const getTopics = (isCurrent, limit) => {
     return new Promise ((resolve, reject) => {
         con.query('CALL get_topic(?, ?)', [isCurrent, limit],  processor.processResults(resolve, reject));
     });
 };
 
-module.exports = { getTopic };
+module.exports = { getTopics };
